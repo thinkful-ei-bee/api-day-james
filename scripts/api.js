bcd8fe7894975b8aba3021ee72d9b869752ddcfe
likejackonfire <gravitybringsmedown@gmail.com>
2019-03-04 12:34:09 -0800

    running test on index.js

--- scripts/api.js
+++ scripts/api.js
@@ -1,27 +1,37 @@
 const api = (function() {
-    const BASE_URL = 'https://thinkful-list-api.herokuapp.com/james';
+    const BASE_URL = ('https://thinkful-list-api.herokuapp.com/jjammess');
 
-    const getItems = function() {
-        fetch(`${BASE_URL}/items`);
+    function getItems() {
+        return fetch(`${BASE_URL}/items`);
         //return Promise.resolve('A successful response!');
     }
 
-    const createItem = function(name) {
+    function createItem(name) {
         let newItem = {
             name,
         };
         return fetch(`${BASE_URL}/items`, {
             method: 'POST',
             body: JSON.stringify(newItem),
-            headers: {'Content-Type': 'application/json' 
-        },
+            headers: new Headers({'Content-Type': 'application/json' 
+            }),
             body: newItem
+        });
+    }
+
+    function updateItem(id, updateData) {
+        return fetch(`${BASE_URL}/items/${id}`,{
+          method: 'PATCH',
+          headers: new Headers({'Content-Type': 'application/json'
+          }),
+          body: JSON.stringify(updateData)
         });
     }
 
     return {
         getItems,
-        createItem
+        createItem,
+        updateItem
     };
 
   })();
--- scripts/index.js
+++ scripts/index.js
@@ -11,4 +11,16 @@ $(document).ready(function() {
   shoppingList.render();
 });
 
+api.getItems()
+  .then(res => res.json())
+  .then((items) => {
+    const item = items[0];
+    return api.updateItem(item.id, { name: 'foobar' });
+  })
+  .then(res => res.json())
+  .then(() => console.log('updated!'));
+
+//fetch('https://thinkful-list-api.herokuapp.com/ei-student/items')
+//.then(res => res.json())
+//.then(data => console.log(data));
 //store.items.push(Item.create('apples'));
