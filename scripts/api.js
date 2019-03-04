e914e28eea41bcc49a2dd8bae1becf7b1ce7a597
likejackonfire <gravitybringsmedown@gmail.com>
2019-03-04 10:56:58 -0800

    added getItem and createItem

--- scripts/api.js
+++ scripts/api.js
@@ -1,4 +1,27 @@
 const api = (function() {
     const BASE_URL = 'https://thinkful-list-api.herokuapp.com/james';
-    alert("IIFE? Maybe.");
+
+    const getItems = function() {
+        fetch(`${BASE_URL}/items`);
+        //return Promise.resolve('A successful response!');
+    }
+
+    const createItem = function(name) {
+        let newItem = {
+            name,
+        };
+        return fetch(`${BASE_URL}/items`, {
+            method: 'POST',
+            body: JSON.stringify(newItem),
+            headers: {'Content-Type': 'application/json' 
+        },
+            body: newItem
+        });
+    }
+
+    return {
+        getItems,
+        createItem
+    };
+
   })();
--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,14 @@
 /* global shoppingList, store */
-
+'use strict';
 $(document).ready(function() {
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
   shoppingList.bindEventListeners();
   shoppingList.render();
 });
 
-store.items.push(Item.create('apples'));
+//store.items.push(Item.create('apples'));
